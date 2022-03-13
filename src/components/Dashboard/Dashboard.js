96474adfb535563ca6e88618f48778158684a1ee
jakubLabedowicz <jalabedowicz@gmail.com>
2022-03-13 17:50:36 +0100

    edit

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,7 +3,24 @@ import './Dashboard.css';
 function Dashboard() {
   return (
     <div className="Dashboard">
-
+      <div className='Dashboard_inner'>
+        <div className='Dashboard_column' id='WIDTH_70'>
+          <div className='Dashboard_section'>
+            Abc
+          </div>
+          <div className='Dashboard_section'>
+            Abc
+          </div>
+        </div>
+        <div className='Dashboard_column' id='WIDTH_30'>
+          <div className='Dashboard_section'>
+            Abc
+          </div>
+          <div className='Dashboard_section'>
+            Abc
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
